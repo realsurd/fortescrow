be490f4d19f9e989e348c6479abc2bf14a599198
sylvamadu <54332482+sylvamadu@users.noreply.github.com>
2024-05-22 01:38:45 +0100

    features landing all done desktop version

---
+++ app/components/LandingPage/mock.ts
@@ -0,0 +1,128 @@
+export const data = [
+  {
+    img: `https://res.cloudinary.com/dlinprg6k/image/upload/v1715838840/Icon_1_kfu1sp.png`,
+    type: `Transaction Initiated`,
+    text: `The buyer initiates the transaction by depositing funds or assets into the escrow account.`,
+  },
+  {
+    img: `https://res.cloudinary.com/dlinprg6k/image/upload/v1715838840/Icon_2_zkcbnb.png`,
+    type: `Seller's Delivery`,
+    text: `The seller provides relevant documentation, tracking information, or proof of delivery to demonstrate fulfillment of their obligations.`,
+  },
+  {
+    img: `https://res.cloudinary.com/dlinprg6k/image/upload/v1715838840/Icon_3_vvkzgi.png`,
+    type: `Buyer’s Confirmation`,
+    text: `Upon receiving the goods or services, the buyer inspects them to ensure they meet the agreed-upon standards and specifications.`,
+  },
+  {
+    img: `https://res.cloudinary.com/dlinprg6k/image/upload/v1715838840/Icon_4_z1gdvl.png`,
+    type: `Escrow Payout`,
+    text: `Once the buyer confirms their satisfaction and approval, the escrow platform releases the funds from escrow to the seller.`,
+  },
+];
+
+export const cases = [
+  {
+    img: `https://res.cloudinary.com/dlinprg6k/image/upload/v1716057559/Icon_Container_mvzi9i.png`,
+    type: `Services`,
+  },
+  {
+    img: `https://res.cloudinary.com/dlinprg6k/image/upload/v1716057561/Icon_Container_1_bwbzvw.png`,
+    type: `Vehicles`,
+  },
+  {
+    img: `https://res.cloudinary.com/dlinprg6k/image/upload/v1716057560/Icon_Container_2_e471nw.png`,
+    type: `Real Estate`,
+  },
+  {
+    img: `https://res.cloudinary.com/dlinprg6k/image/upload/v1716057560/Icon_Container_3_vwnztj.png`,
+    type: `Milestones`,
+  },
+];
+export const options = [
+  {
+    title: `Secure and Reliable Transactions:`,
+    text: `At ForteScrow, we prioritize the security and reliability of your transactions. Our advanced escrow services ensure that your funds or assets are securely held until all conditions of the transaction are met, providing you with peace of mind throughout the process.`,
+  },
+  {
+    title: `Trusted Escrow Partner:`,
+    text: `With ForteScrow, you have a trusted escrow partner by your side. Our platform operates as a neutral third party, facilitating fair and transparent transactions between buyers and sellers. You can trust us to protect your interests and ensure a smooth transaction experience.`,
+  },
+  {
+    title: `Ease of Use:`,
+    text: `We've designed ForteScrow with simplicity and convenience in mind. Our user-friendly interface makes it easy to initiate transactions, track progress, and communicate with the other party. Whether you're a seasoned trader or new to escrow services, ForteScrow is accessible to all users.`,
+  },
+  {
+    title: `24/7 Customer Support:`,
+    text: `Your satisfaction is our priority. That's why we offer 24/7 customer support to assist you at every step of the way. Whether you have questions about a transaction, need assistance with account management, or require help resolving a dispute, our dedicated support team is here to help.`,
+  },
+];
+export const faqs = [
+  {
+    title: `What is Fortescrow really all about?`,
+    text: `Tyms is a comprehensive accounting and finance automation software for businesses. We understand that accounting can be very challenging, and as a business owner or finance expert, you need a faster and smarter way to keep track and generate the necessary reports and insights in real-time. Therefore, Tyms automates and makes managing your finances flexible and easy.`,
+  },
+  {
+    title: `Why do I need an escrow platform?`,
+    text: `Tyms is a comprehensive accounting and finance automation software for businesses. We understand that accounting can be very challenging, and as a business owner or finance expert, you need a faster and smarter way to keep track and generate the necessary reports and insights in real-time. Therefore, Tyms automates and makes managing your finances flexible and easy.`,
+  },
+  {
+    title: `What payment methods does Fortescrow support?`,
+    text: `Tyms is a comprehensive accounting and finance automation software for businesses. We understand that accounting can be very challenging, and as a business owner or finance expert, you need a faster and smarter way to keep track and generate the necessary reports and insights in real-time. Therefore, Tyms automates and makes managing your finances flexible and easy.`,
+  },
+  {
+    title: `How can I contact ForteScrow’s customer support team?`,
+    text: `Tyms is a comprehensive accounting and finance automation software for businesses. We understand that accounting can be very challenging, and as a business owner or finance expert, you need a faster and smarter way to keep track and generate the necessary reports and insights in real-time. Therefore, Tyms automates and makes managing your finances flexible and easy.`,
+  },
+  {
+    title: `Why should I choose Fortescrow?`,
+    text: `Tyms is a comprehensive accounting and finance automation software for businesses. We understand that accounting can be very challenging, and as a business owner or finance expert, you need a faster and smarter way to keep track and generate the necessary reports and insights in real-time. Therefore, Tyms automates and makes managing your finances flexible and easy.`,
+  },
+  {
+    title: `What happens when there’s dispute between a buyer and a seller?`,
+    text: `Tyms is a comprehensive accounting and finance automation software for businesses. We understand that accounting can be very challenging, and as a business owner or finance expert, you need a faster and smarter way to keep track and generate the necessary reports and insights in real-time. Therefore, Tyms automates and makes managing your finances flexible and easy.`,
+  },
+  {
+    title: `Can I dispute a transaction on Fortescrow if the goods or services do not meet my expectations?`,
+    text: `Tyms is a comprehensive accounting and finance automation software for businesses. We understand that accounting can be very challenging, and as a business owner or finance expert, you need a faster and smarter way to keep track and generate the necessary reports and insights in real-time. Therefore, Tyms automates and makes managing your finances flexible and easy.`,
+  },
+  {
+    title: `Can I try Fortescrow for free?`,
+    text: `Tyms is a comprehensive accounting and finance automation software for businesses. We understand that accounting can be very challenging, and as a business owner or finance expert, you need a faster and smarter way to keep track and generate the necessary reports and insights in real-time. Therefore, Tyms automates and makes managing your finances flexible and easy.`,
+  },
+  {
+    title: `Can I cancel a transaction on ForteScrow?`,
+    text: `Tyms is a comprehensive accounting and finance automation software for businesses. We understand that accounting can be very challenging, and as a business owner or finance expert, you need a faster and smarter way to keep track and generate the necessary reports and insights in real-time. Therefore, Tyms automates and makes managing your finances flexible and easy.`,
+  },
+  {
+    title: `How long does it take to complete a transaction on Fortescrow?`,
+    text: `Tyms is a comprehensive accounting and finance automation software for businesses. We understand that accounting can be very challenging, and as a business owner or finance expert, you need a faster and smarter way to keep track and generate the necessary reports and insights in real-time. Therefore, Tyms automates and makes managing your finances flexible and easy.`,
+  },
+];
+
+export const testimonials = [
+  {
+    img: `https://res.cloudinary.com/dlinprg6k/image/upload/v1716248132/Image_jjq2xo.png`,
+    name: `John Anderson`,
+    title: `Entrepreneur`,
+    text: `"I've been using ForteScrow for all my online transactions, and I couldn't be happier! The platform's security measures gave me the confidence to buy expensive items without worrying about scams. Highly recommended!"`,
+  },
+  {
+    img: `https://res.cloudinary.com/dlinprg6k/image/upload/v1716248132/Image_1_ety96j.png`,
+    name: `Sarah Johnson`,
+    title: `CEO Boutique`,
+    text: `"As a seller, ForteScrow has been a game-changer for me. I no longer have to deal with the stress of fraudulent buyers or chargebacks. Thanks to ForteScrow, I can focus on growing my business without worrying about payment issues."`,
+  },
+  {
+    img: `https://res.cloudinary.com/dlinprg6k/image/upload/v1716248133/Image_2_ojuxbw.png`,
+    name: `Emily Turner`,
+    title: `Founder EventMasters`,
+    text: `"ForteScrow's customer support team went above and beyond to help me resolve a dispute with a seller. Their professionalism and dedication to customer satisfaction are truly commendable. I'll definitely continue using ForteScrow for all my future transactions!"`,
+  },
+  {
+    img: `https://res.cloudinary.com/dlinprg6k/image/upload/v1716248132/Image_jjq2xo.png`,
+    name: `James White`,
+    title: `Co-Founder`,
+    text: `"ForteScrow's customer support team went above and beyond to help me resolve a dispute with a seller. Their professionalism and dedication to customer satisfaction are truly commendable. I'll definitely continue using ForteScrow for all my future transactions!"`,
+  },
+];
