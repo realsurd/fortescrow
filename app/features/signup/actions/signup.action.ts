ce4dfc4948d35976aeb553751666cefa1892547d
sylvamadu <54332482+sylvamadu@users.noreply.github.com>
2024-07-30 01:37:06 +0100

    integrated sign up

---
+++ app/features/signup/actions/signup.action.ts
@@ -0,0 +1,33 @@
+import { useNotify } from '@/hooks';
+import { useFetchWrapper } from '@/hooks/useFetchWrapper';
+import { useCallback } from 'react';
+import { useSetRecoilState } from 'recoil';
+// import { ProposalsAtom, VotesAtom } from '../state/governance.atom';
+import { RegisterUserDto } from '@/interfaces';
+
+export const useSignUpActions = () => {
+  const fetchWrapper = useFetchWrapper();
+  // const setProposals = useSetRecoilState(ProposalsAtom);
+  // const setVotes = useSetRecoilState(VotesAtom);
+  const { notify } = useNotify();
+
+
+  const registerUser = useCallback(async (dto: RegisterUserDto) => {
+    try {
+      const response = await fetchWrapper.post(
+        'users/register/',
+        dto,
+      );
+
+      return response;
+    } catch (error) {
+      return {
+        error,
+      };
+    }
+  }, []);
+
+  return {
+    registerUser,
+  };
+};
