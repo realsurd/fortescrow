d4193eee38388eca0d9412e3d2b3d18faffaf14f
Adeyemi Sadiq <adexsadiq01@gmail.com>
2024-08-05 20:55:20 +0100

    mobile design

--- app/components/LoginPage/index.tsx
+++ app/components/LoginPage/index.tsx
@@ -15,7 +15,7 @@ export function LoginPage() {
 
   return (
     <div className="flex justify-between font-space-grotesk items-start w-full h-[100vh] mb-10 overflow-y-auto">
-      <div className="w-[45%] min-h-[100vh] bg-cover bg-center relative">
+      <div className="hidden md:block w-[45%] min-h-[100vh] bg-cover bg-center relative">
         <Link href={'/'}>
           <h1 className="text-center m-3 flex bg-white justify-center items-center font-bold text-[#006877]">
             ForteScrow
@@ -31,7 +31,7 @@ export function LoginPage() {
           <div className="flex w-full">
             <div className="flex flex-col justify-start items-start w-full mt-2 mr-5 mb-2 ml-5 p-3 leading-3">
               <div className="mb-1 flex justify-center items-center font-space-grotesk">
-                <div className=" flex flex-col">
+                <div className="flex flex-col">
                   <div className="flex items-center">
                     <h1 className="w-8 h-8 flex justify-center text-sm items-center bg-[#f5f5f5] rounded-full font-bold text-[#615D61]">
                       01
@@ -144,98 +144,124 @@ export function LoginPage() {
         </div>
       </div>
 
-      {/* Login*/}
-      <div className="w-full md:w-[100%] mx-20 my-[70px] font-inter">
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="font-bold text-3xl text-[#333333]">Login</h1>
-          <p className="text-sm m-2 text-[#4C4C4C]">
-            Welcome back! Please log in to access your account.
-          </p>
-        </div>
-
-        <div className="flex flex-col items-center justify-center mx-auto my-5 md:w-[70%]">
-          <div className="flex flex-col w-full">
-            <label htmlFor="email" className="text-[12px] m-1 text-[#333333]">
-              Email
-            </label>
-            <input
-              type="email"
-              className="h-10 border text-[13px] border-gray-300 mt-1 mb-2 rounded-md p-4 w-full bg-gray-50 focus:outline-none focus:border-[#006877] focus:ring-1 focus:ring-[#006877] focus:shadow-sm focus:shadow-white"
-              placeholder="Userexample@gmail.com"
-            />
-          </div>
-
-          <div className="flex flex-col mb-5 w-full">
-            <div className="flex justify-between w-full md:w-full mr-0 md:mr-10 flex-col">
-              <label htmlFor="password" className="text-[12px] text-black">
-                Password
-              </label>
-              <input
-                type="password"
-                className="h-10 border text-[13px] border-gray-300 mt-1 rounded-md p-4 w-full bg-gray-50 focus:outline-none focus:border-[#006877] focus:ring-1 focus:ring-[#006877] focus:shadow-sm focus:shadow-white"
-                placeholder="Enter your password"
-              />
-              <p className="flex justify-end items-end font-inter text-[#4C4C4C] text-sm">
-                forgot password?
-              </p>
-            </div>
-          </div>
-
-          <div className="w-full mb-5">
-            <div className="flex items-center">
-              <input type="checkbox" id="terms" name="terms" />
-              <p className="text-sm ml-2">Remember me</p>
-            </div>
-          </div>
-
-          <div className="w-full">
-            <button className="p-3 border w-full items-center bg-custom-gradient text-white rounded-md">
+      {/* Login */}
+      <div
+        className={`w-[100%] min-h-screen ${
+          isMobile ? 'bg-diagonal-gradients' : 'bg-white'
+        } flex flex-col items-center justify-center`}
+      >
+        {isMobile && (
+          <Link href={'/'}>
+            <h1 className="mt-10 flex justify-center items-center font-bold text-[#006877] text-3xl font-space-grotesk">
+              ForteScrow
+            </h1>
+          </Link>
+        )}
+        <div className="w-[100%] min-h-screen p-2 rounded-tl-3xl rounded-tr-3xl md:w-[100%] mx-5 my-[40px] bg-white font-inter">
+          <div className="flex flex-col justify-center items-center text-center">
+            <h1 className="font-bold font-space-grotesk text-3xl text-[#006877] md:text-[#333333]">
               Login
-            </button>
-            <p className="flex justify-end items-center font-inter text-[#4C4C4C] text-sm">
-              {`Don't have an account?`}
-              <Link href={'/signup'}>
-                <span className="flex items-center ml-2 justify-center">
-                  Sign Up <GoArrowUpRight className="w-5" />
-                </span>
-              </Link>
+            </h1>
+            <p className="text-sm font-avenir w-[250px] md:w-full text-center m-2 text-[#4C4C4C]">
+              Welcome back! Please log in to access your account.
             </p>
           </div>
-          <div className="w-full m-3">
-            <div className="flex items-center">
-              <div className="flex-grow border-t border-gray-100"></div>
-              <span className="mx-4 text-sm text-[#4C4C4C]">OR</span>
-              <div className="flex-grow border-t border-gray-100"></div>
-            </div>
-          </div>
-        </div>
-        <div className="flex justify-between items-center w-[50%]">
-          <div className="flex items-center align-center justify-center">
-            <div className="flex items-center justify-center ml-[160px] p-3 w-[100px] bg-[#F7FAFF] rounded-md">
-              <img
-                src="https://res.cloudinary.com/dlinprg6k/image/upload/v1716163547/googleLogo_knrnzk.png"
-                width={50}
-                className="w-[30px]"
-              />
-            </div>
-          </div>
-          <div className="flex items-center align-center justify-center">
-            <div className="flex items-center justify-center ml-[50px] p-3 w-[100px] bg-[#F7FAFF] rounded-md">
-              <img
-                src="https://res.cloudinary.com/dlinprg6k/image/upload/v1716171724/fb_logo_edu1b5.png"
-                width={50}
-                className="w-[50px]"
-              />
-            </div>
-          </div>
 
-          <div className="flex items-center align-center justify-center">
-            <div className="flex items-center justify-center ml-[50px] p-3 w-[100px] bg-[#F7FAFF] rounded-md">
-              <img
-                src="https://res.cloudinary.com/dlinprg6k/image/upload/v1716172290/twitter_logo_w0bp6d.png"
-                width={50}
-                className="w-[50px]"
+          <div className="flex flex-col items-center justify-center mx-auto my-5 md:w-[70%]">
+            <div className="mb-10 md:mb-4 flex flex-col w-full ">
+              <label
+                htmlFor="email"
+                className="text-[12px] m-1 text-[#333333] font-bold"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                className="h-10 border text-[13px] border-gray-300 mt-1 rounded-md p-8 md:p-4 w-full bg-gray-50 focus:outline-none focus:border-[#A4EEFF] focus:ring-1 focus:ring-[#A4EEFF] focus:shadow-sm focus:shadow-white"
+                placeholder="Userexample@gmail.com"
               />
+            </div>
+
+            <div className="flex flex-col mb-5 w-full">
+              <div className="flex justify-between w-full md:w-full mr-0 md:mr-10 flex-col">
+                <label
+                  htmlFor="password"
+                  className="text-[12px] text-[#333333] font-bold"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="h-10 border text-[13px] border-gray-300 mt-1 rounded-md p-8 md:p-4 w-full bg-gray-50 focus:outline-none focus:border-[#A4EEFF] focus:ring-1 focus:ring-[#A4EEFF] focus:shadow-sm focus:shadow-white"
+                  placeholder="Enter your password"
+                />
+                <p className="flex justify-end items-end font-inter text-[#4C4C4C] mt-3 md:mt-2 text-sm">
+                  forgot password?
+                </p>
+              </div>
+            </div>
+
+            <div className="w-full mb-5">
+              <div className="flex items-center">
+                <input
+                  type="checkbox"
+                  id="terms"
+                  name="terms"
+                  className="p-4 rounded-md bg-white border-[#FFDAD6] shadow-sm"
+                />
+                <p className="text-sm ml-2">Remember me</p>
+              </div>
+            </div>
+
+            <div className="w-full">
+              <button className="p-7 font-space-grotesk font-semibold md:p-4 border w-full items-center bg-custom-gradient text-white rounded-2xl md:rounded-md">
+                Login
+              </button>
+              <p className="flex justify-end items-center font-inter m-3 md:m-5 text-[#4C4C4C] text-sm">
+                {`Don't have an account?`}
+                <Link href={'/signup'}>
+                  <span className="flex underline underline-offset-1 font-bold items-center ml-2 justify-center">
+                    Sign Up <GoArrowUpRight className="w-5" />
+                  </span>
+                </Link>
+              </p>
+            </div>
+            <div className="w-full">
+              <div className="flex items-center">
+                <div className="flex-grow border-t border-gray-100"></div>
+                <span className="mx-4 text-sm text-[#4C4C4C]">OR</span>
+                <div className="flex-grow border-t border-gray-100"></div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between items-center mx-[25px] mt-20 md:mx-[80px] md:w-[50%]">
+            <div className="flex items-center align-center justify-center">
+              <div className="flex items-center justify-center md:ml-[160px] p-3 w-[100px] bg-[#F7FAFF]  rounded-md">
+                <img
+                  src="https://res.cloudinary.com/dlinprg6k/image/upload/v1716163547/googleLogo_knrnzk.png"
+                  width={50}
+                  className="w-[40px]"
+                />
+              </div>
+            </div>
+            <div className="flex items-center align-center justify-center">
+              <div className="flex items-center justify-center ml-[50px] p-3 w-[100px] bg-[#F7FAFF] rounded-md">
+                <img
+                  src="https://res.cloudinary.com/dlinprg6k/image/upload/v1716171724/fb_logo_edu1b5.png"
+                  width={50}
+                  className="w-[60px]"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center align-center justify-center">
+              <div className="flex items-center justify-center ml-[50px] p-3 w-[100px] bg-[#F7FAFF] rounded-md">
+                <img
+                  src="https://res.cloudinary.com/dlinprg6k/image/upload/v1716172290/twitter_logo_w0bp6d.png"
+                  width={50}
+                  className="w-[60px]"
+                />
+              </div>
             </div>
           </div>
         </div>
--- app/components/VerificationPage/index.tsx
+++ app/components/VerificationPage/index.tsx
@@ -1,98 +1,112 @@
 import { useWindowDimensions } from '../../hooks/useWindowDimensions';
-import { useState } from 'react';
+import Link from 'next/link';
 import styles from './index.module.scss';
 
 export function VerificationPage() {
-  // const [activeDropDown, setActiveDropDown] = useState(false);
-  // const [activeDropDownTwo, setActiveDropDownTwo] = useState(false);
-  // const [openSideNav, setOpenSideNav] = useState(false);
-
   const { width } = useWindowDimensions();
   const isMobile = width ? width < 768 : false;
 
   return (
-    <div className="flex max-w-full items-center justify-center m-10">
-      <div className="max-w-full border shadow-lg mt-20 rounded-lg">
-        <div className="m-[50px] items-center">
-          <h1 className="text-center font-bold text-2xl mb-2 text-[#006877] font-lexend">
-            Verification
+    <div className="relative">
+      {isMobile && (
+        <img
+          src="https://res.cloudinary.com/dlinprg6k/image/upload/v1715559930/fort-background_jyso4x.jpg"
+          alt="Background"
+          className="w-full t-0 min-h-screen object-cover absolute opacity-20"
+        />
+      )}
+      {isMobile && (
+        <Link href={'/'}>
+          <h1 className="mt-10 flex justify-center items-center font-bold text-[#006877] text-3xl font-space-grotesk">
+            ForteScrow
           </h1>
-          <p className="text-sm text-center leading-6 text-[#615D61] font-avenir">
-            A six digit code has been sent via SMS to your number that ends with
-            <span className="font-semibold">*5499</span>. <br /> Kindly confirm
-            and provide the code in the input field below.{' '}
-          </p>
-        </div>
-        <form className="px-4  m-10">
-          <div className="flex justify-center gap-2 mb-6">
-            <input
-              className="w-12 font-bold text-[#615D61] text-2xl font-space-grotesk h-12 text-center border-[#AFAAAE] rounded-md shadow-sm focus:border-[#A4EEFF] focus:ring-[#A4EEFF]"
-              type="text"
-              maxLength="1"
-              pattern="[0-9]"
-              inputMode="numeric"
-              autoComplete="one-time-code"
-              required
-            />
-            <input
-              className="w-12 font-bold text-[#615D61] text-2xl font-space-grotesk h-12 text-center border-[#AFAAAE] rounded-md shadow-sm focus:border-[#A4EEFF] focus:ring-[#A4EEFF]"
-              type="text"
-              maxLength="1"
-              pattern="[0-9]"
-              inputMode="numeric"
-              autoComplete="one-time-code"
-              required
-            />
-            <input
-              className="w-12 font-bold text-[#615D61] text-2xl font-space-grotesk h-12 text-center border-[#AFAAAE] rounded-md shadow-sm focus:border-[#A4EEFF] focus:ring-[#A4EEFF]"
-              type="text"
-              maxLength="1"
-              pattern="[0-9]"
-              inputMode="numeric"
-              autoComplete="one-time-code"
-              required
-            />
-            <input
-              className="w-12 font-bold text-[#615D61] text-2xl font-space-grotesk h-12 text-center border-[#AFAAAE] rounded-md shadow-sm focus:border-[#A4EEFF] focus:ring-[#A4EEFF]"
-              type="text"
-              maxLength="1"
-              pattern="[0-9]"
-              inputMode="numeric"
-              autoComplete="one-time-code"
-              required
-            />
-            <input
-              className="w-12 font-bold text-[#615D61] text-2xl font-space-grotesk h-12 text-center border-[#AFAAAE] rounded-md shadow-sm focus:border-[#A4EEFF] focus:ring-[#A4EEFF]"
-              type="text"
-              maxLength="1"
-              pattern="[0-9]"
-              inputMode="numeric"
-              autoComplete="one-time-code"
-              required
-            />
-            <input
-              className="w-12 font-bold text-[#615D61] text-2xl font-space-grotesk h-12 text-center border-[#AFAAAE] rounded-md shadow-sm focus:border-[#A4EEFF] focus:ring-[#A4EEFF]"
-              type="text"
-              maxLength="1"
-              pattern="[0-9]"
-              inputMode="numeric"
-              autoComplete="one-time-code"
-              required
-            />
-          </div>
-          <div className="flex flex-col justify-center">
-            <button
-              className="w-full bg-custom-gradient text-white font-space-grotesk font-bold py-2 px-4 rounded-md"
-              type="button"
-            >
-              Verify OTP
-            </button>
-            <p className="flex justify-end items-center m-2 font-lexend text-[#4C4C4C] text-[12px]">
-              Didn’t receive an OTP?
-              <span className="text-[#006877] font-bold">Resend OTP</span>
+        </Link>
+      )}
+      <div className="relative flex max-w-full items-center justify-center m-5">
+        <div className="max-w-full border shadow-2xl mt-20 rounded-3xl bg-white">
+          <div className="m-[50px] items-center">
+            <h1 className="text-center font-bold text-2xl mb-2 text-[#006877] font-lexend">
+              Verification
+            </h1>
+            <p className="w-[250px] text-center md:w-full text-sm md:text-center text-[#b58985] font-avenir">
+              A six digit code has been sent via SMS to your number that ends
+              with
+              <span className="font-semibold">*5499</span>.Kindly <br /> confirm
+              and provides the code in the input field below.{' '}
             </p>
           </div>
-        </form>
+
+          <form className="px-4 m-10">
+            <div className="flex justify-center gap-2 mb-6">
+              <input
+                className="w-12 font-bold text-[#615D61] text-2xl font-space-grotesk h-12 text-center border-[#AFAAAE] rounded-md shadow-sm focus:border-[#A4EEFF] focus:ring-[#A4EEFF]"
+                type="text"
+                maxLength={1}
+                pattern="[0-9]"
+                inputMode="numeric"
+                autoComplete="one-time-code"
+                required
+              />
+              <input
+                className="w-12 font-bold text-[#615D61] text-2xl font-space-grotesk h-12 text-center border-[#AFAAAE] rounded-md shadow-sm focus:border-[#A4EEFF] focus:ring-[#A4EEFF]"
+                type="text"
+                maxLength={1}
+                pattern="[0-9]"
+                inputMode="numeric"
+                autoComplete="one-time-code"
+                required
+              />
+              <input
+                className="w-12 font-bold text-[#615D61] text-2xl font-space-grotesk h-12 text-center border-[#AFAAAE] rounded-md shadow-sm focus:border-[#A4EEFF] focus:ring-[#A4EEFF]"
+                type="text"
+                maxLength={1}
+                pattern="[0-9]"
+                inputMode="numeric"
+                autoComplete="one-time-code"
+                required
+              />
+              <input
+                className="w-12 font-bold text-[#615D61] text-2xl font-space-grotesk h-12 text-center border-[#AFAAAE] rounded-md shadow-sm focus:border-[#A4EEFF] focus:ring-[#A4EEFF]"
+                type="text"
+                maxLength={1}
+                pattern="[0-9]"
+                inputMode="numeric"
+                autoComplete="one-time-code"
+                required
+              />
+              <input
+                className="w-12 font-bold text-[#615D61] text-2xl font-space-grotesk h-12 text-center border-[#AFAAAE] rounded-md shadow-sm focus:border-[#A4EEFF] focus:ring-[#A4EEFF]"
+                type="text"
+                maxLength={1}
+                pattern="[0-9]"
+                inputMode="numeric"
+                autoComplete="one-time-code"
+                required
+              />
+              <input
+                className="w-12 font-bold text-[#615D61] text-2xl font-space-grotesk h-12 text-center border-[#AFAAAE] rounded-md shadow-sm focus:border-[#A4EEFF] focus:ring-[#A4EEFF]"
+                type="text"
+                maxLength={1}
+                pattern="[0-9]"
+                inputMode="numeric"
+                autoComplete="one-time-code"
+                required
+              />
+            </div>
+            <div className="flex flex-col justify-center">
+              <button
+                className="w-full bg-custom-gradient text-white font-space-grotesk font-bold p-5 md:p-4 rounded-lg"
+                type="button"
+              >
+                Verify OTP
+              </button>
+              <p className="flex justify-end items-center mt-3 font-lexend text-[#B3B3B3] text-[12px]">
+                Didn’t receive an OTP?
+                <span className="text-[#006877] font-bold">Resend OTP</span>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
